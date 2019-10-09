453733114809c312a33104b1fb9131d02db414f1
Ragavi Venkatesh <ragavivenkatesh2121@gmail.com>
2019-10-09 16:23:22 +0530

    My setup is successful.

---
+++ ShoppingBackend/src/main/java/Ragavi/net/ShoppingBackend/App.java
@@ -0,0 +1,13 @@
+package Ragavi.net.ShoppingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
